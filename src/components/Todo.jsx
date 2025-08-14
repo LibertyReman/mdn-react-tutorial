b070934db7a9b4ccb12d146f1abd73ae2b281cf0
LibertyReman <libertyreman@gmail.com>
2025-08-14 16:31:13 +0900

    Todoコンポーネントの作成2

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,19 @@
-
-function Todo() {
+function Todo(props) {
+  console.log(props);
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id="todo-0" type="checkbox" defaultChecked />
+        <input id="todo-0" type="checkbox" defaultChecked={true} />
         <label className="todo-label" htmlFor="todo-0">
-          Eat
+          {props.name}
         </label>
       </div>
       <div className="btn-group">
         <button type="button" className="btn">
-          Edit <span className="visually-hidden">Eat</span>
+          Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button type="button" className="btn btn__danger">
-          Delete <span className="visually-hidden">Eat</span>
+          Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
     </li>
@@ -21,3 +21,4 @@ function Todo() {
 }
 
 export default Todo;
+
