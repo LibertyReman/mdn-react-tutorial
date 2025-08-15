99fc88d905ff99a32372f60b570c8f5510d1cca8
LibertyReman <libertyreman@gmail.com>
2025-08-15 10:15:18 +0900

    ユーザー入力の読み取り

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,24 @@ import { useState } from "react";
 function Form(props) {
   // useState() は、 2 つのアイテムを含む配列を返します。
   // 最初のアイテムは現在の状態の値であり、 2 番目のアイテムは状態を更新するために使用できる関数です。
-  const [name, setName] = useState("Learn React");
+  const [name, setName] = useState("");
+
+  // 入力時に呼ばれる
+  function handleChange(event) {
+    console.log(event.target.value);
+    // 状態の更新
+    setName(event.target.value);
+  }
+
   // formのAddボタン押下時に呼び出し
   function handleSubmit(event) {
     // デフォルト動作の無効化
     event.preventDefault();
-    alert("Hello, world!");
-    props.addTask("Say hello!"); // App.jsxのaddTask関数を呼び出し
+    if (!name) return;
+
+    //alert("Hello, world!");
+    props.addTask(name); // App.jsxのaddTask関数を呼び出し
+    setName(""); // nameの初期化
   }
 
   return (
@@ -19,6 +30,7 @@ function Form(props) {
           What needs to be done?
         </label>
       </h2>
+      {/* nameにはsetNameで更新された値が入る */}
       <input
         type="text"
         id="new-todo-input"
@@ -26,6 +38,7 @@ function Form(props) {
         name="text"
         autoComplete="off"
         value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
